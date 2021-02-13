f929a8e20764a0cae6f05d9b4a7f36052c03d7b8
gunimus <gunimus@overspec.net>
2021-02-13 11:27:51 +0000

    Fix command error

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,12 @@
 import os
-import time
-import traceback
-import datetime
-import requests
+import asyncio
+import random
 from discord.ext import commands
 from script import common
 from script import speedrun
-import random
 
 bot = commands.Bot(command_prefix='!')
 token = os.environ['DISCORD_BOT_TOKEN']
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
 
 @bot.command()
 async def schedule(ctx):
@@ -90,15 +81,15 @@ async def hanabi(ctx):
 @bot.command()
 async def c(ctx):
     await ctx.send('The race will begin in 10 seconds!')
-    time.sleep(5)
+    await asyncio.sleep(5)
     await ctx.send('The race will begin in 5 seconds!')
-    time.sleep(2)
+    await asyncio.sleep(2)
     await ctx.send("3")
-    time.sleep(1)
+    await asyncio.sleep(1)
     await ctx.send("2")
-    time.sleep(1)
+    await asyncio.sleep(1)
     await ctx.send("1")
-    time.sleep(1)
+    await asyncio.sleep(1)
     await ctx.send("Go!")
 
 @bot.command()
--- readme.md
+++ readme.md
@@ -1,25 +1,11 @@
-RTABot
+RTAStoreBot
 ====
 
-Discordに追加できるBOTです。  
-RTAに便利なコマンドをまとめています。
+運営しているRTAStoreの為に作ったBOTです。
 
 # Install
 
  [こちら](https://discord.com/api/oauth2/authorize?client_id=708260795284193301&permissions=0&scope=bot)にアクセスして追加したいサーバーを選択して追加します。
-
-# How to use
-## CountDown
-コマンドは`!c`  
-10秒前からカウントし、  
-5秒前、3、2、1、Go!とメッセージを返します。  
-並走などをする際にお使いください。
-
-## Ranking
-※テキストチャンネルの名前を「記録」「きろく」「record」のいずれかが含まれるようにしてください。  
-コマンドは`!ranking {speedrun.comのユーザー名}`  
-speedrun.comのユーザー名からRefunctのカテゴリーと順位を返します。  
-自分、または他の走者のランキング一覧が取得できます。
 
 # Author
 連絡先はこちらまで  
